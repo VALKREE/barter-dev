f373104ddc3532a51d84b8a1e72357b282e005bd
VALKREE <49433750+VALKREE@users.noreply.github.com>
2022-08-12 10:41:56 +0400

    1

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cl from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={cl.footer}>
+
+        </footer>
+    );
+};
+
+export default Footer;
